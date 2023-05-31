99863c30f4b5ca0f6d07afeb0351e7a2f0482dba
mibauza <matiasibauza@gmail.com>
2023-05-31 12:15:41 -0300

    modifico algo para pushear de nuevo

---
+++ templates/UL_pc_viewAll.tpl
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <base href={$url}>
+    <title>Visualizando PC: {$pc_arreglo[0]->name}</title>
+</head>
+<body>
+    {include file= "UL_header.tpl"}
+    <main class="main">
+    {include file= "UL_aside.tpl"}
+    <section>
+        <div class="seccionPc">
+        {foreach from=$pc_arreglo item=$elemento}
+        <article>
+        <img src="https://s3-sa-east-1.amazonaws.com/saasargentina/JsxkDHYTeoqs0meqg4G2/imagen" alt="PC ARMADA">
+            <a href="detail/{$elemento->id_pc}">
+                <ul>                
+                <li>ID PC: {$elemento->id_pc}</li>
+                    <li>Motherboard: {$elemento->motherboard}</li>
+                    <li>Processor: {$elemento->processor}</li>
+                    <li>Video Card: {$elemento->video}</li>
+                    <li>RAM: {$elemento->RAM}</li>
+                    <li>Gama: {$elemento->name_gama}</li>
+                </ul>
+            </a>
+            <a href="editarPc/{$elemento->id_pc}">Edit</a>
+            <a href="eliminarPc/$elemento->id_pc">Delete</a>
+        </article>
+        {/foreach}
+    </div>
+    </section>
+    </main>
+    {include file= "footer.tpl"}
+</body>
+</html>
---
+++ templates_c/22cab1f60657594e66db5fa3e9d2d59a44bba5ce_0.file.Ul_pc_viewAll.tpl.php
@@ -0,0 +1,86 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-05-31 16:52:52
+  from 'C:\xampp\htdocs\proyectos\TPE-WEB2\templates\Ul_pc_viewAll.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_64775f44ef1319_20725617',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '22cab1f60657594e66db5fa3e9d2d59a44bba5ce' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\proyectos\\TPE-WEB2\\templates\\Ul_pc_viewAll.tpl',
+      1 => 1685544771,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:UL_header.tpl' => 1,
+    'file:UL_aside.tpl' => 1,
+    'file:footer.tpl' => 1,
+  ),
+),false)) {
+function content_64775f44ef1319_20725617 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <base href=<?php echo $_smarty_tpl->tpl_vars['url']->value;?>
+>
+    <title>Visualizando PC: <?php echo $_smarty_tpl->tpl_vars['pc_arreglo']->value[0]->name;?>
+</title>
+</head>
+<body>
+    <?php $_smarty_tpl->_subTemplateRender("file:UL_header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+    <main class="main">
+    <?php $_smarty_tpl->_subTemplateRender("file:UL_aside.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+    <section>
+        <div class="seccionPc">
+        <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['pc_arreglo']->value, 'elemento');
+$_smarty_tpl->tpl_vars['elemento']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['elemento']->value) {
+$_smarty_tpl->tpl_vars['elemento']->do_else = false;
+?>
+        <article>
+        <img src="https://s3-sa-east-1.amazonaws.com/saasargentina/JsxkDHYTeoqs0meqg4G2/imagen" alt="PC ARMADA">
+            <a href="detail/<?php echo $_smarty_tpl->tpl_vars['elemento']->value->id_pc;?>
+">
+                <ul>                
+                <li>ID PC: <?php echo $_smarty_tpl->tpl_vars['elemento']->value->id_pc;?>
+</li>
+                    <li>Motherboard: <?php echo $_smarty_tpl->tpl_vars['elemento']->value->motherboard;?>
+</li>
+                    <li>Processor: <?php echo $_smarty_tpl->tpl_vars['elemento']->value->processor;?>
+</li>
+                    <li>Video Card: <?php echo $_smarty_tpl->tpl_vars['elemento']->value->video;?>
+</li>
+                    <li>RAM: <?php echo $_smarty_tpl->tpl_vars['elemento']->value->RAM;?>
+</li>
+                    <li>Gama: <?php echo $_smarty_tpl->tpl_vars['elemento']->value->name_gama;?>
+</li>
+                </ul>
+            </a>
+            <a href="editarPc/<?php echo $_smarty_tpl->tpl_vars['elemento']->value->id_pc;?>
+">Edit</a>
+            <a href="eliminarPc/$elemento->id_pc">Delete</a>
+        </article>
+        <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+    </div>
+    </section>
+    </main>
+    <?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+</body>
+</html><?php }
+}
